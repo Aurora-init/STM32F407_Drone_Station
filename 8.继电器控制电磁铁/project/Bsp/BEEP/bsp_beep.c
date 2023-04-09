3af01d9f458549605d680d40eb4e8715b1d304a5
Aurora_init <1114813380@qq.com>
2023-04-09 11:32:10 +0800

    未进行代码优化的功能完成版本的代码

---
+++ 8.继电器控制电磁铁/project/Bsp/BEEP/bsp_beep.c
@@ -0,0 +1,21 @@
+#include "COMMON/bsp_device.h"
+#include "COMMON/bsp_common.h"	
+#include "COMMON/bsp_headfile.h"	
+#include "main.h"
+//蜂鸣器
+
+
+/*例子：
+
+		Beep_On;//打开蜂鸣器
+		for(int i=0;i<10;i++)
+		{
+			HAL_Delay(1000);//延时
+		}
+		Beep_Off;//关闭蜂鸣器
+		for(int i=0;i<10;i++)
+		{
+			HAL_Delay(1000);//延时
+		}
+		
+*/
---
+++ 8.继电器控制电磁铁/project/Bsp/MOTOR/bsp_motor.h
@@ -0,0 +1,15 @@
+#ifndef _BSP_MOTOR_H_
+#define _BSP_MOTOR_H_
+
+void MOTOR_RUN1(void);
+void MOTOR_RUN2(void);
+void MOTOR_RUN3(void);
+void MOTOR_RUN4(void);
+void MOTOR_RUN5(void);
+void MOTOR_RUN6(void);
+void MOTOR_RUN7(void);
+void MOTOR_RUN8(void);
+void MOTOR_RUN9(void);
+void MOTOR_RUNx(void);
+
+#endif
